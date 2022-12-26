906cf0132b3c150255d0b33a333b428e3814010e
Phuc1112004 <phuctv1112004@gmail.com>
2022-12-26 10:50:07 +0700

    baithi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FavoritesComponent} from "./page/favorites/favorites.component";
+import { JohnComponent} from "./page/john/john.component";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+
+
+const appRoutes: Routes = [
+  {path: 'favorites',component:FavoritesComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,FavoritesComponent ,JohnComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
