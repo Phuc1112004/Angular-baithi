7da599692baf85a54f314fede1d84d540bd58501
Phuc1112004 <phuctv1112004@gmail.com>
2022-12-26 11:15:55 +0700

    baithi

--- src/app/page/favorites/favorites.component.ts
+++ src/app/page/favorites/favorites.component.ts
@@ -1,10 +1,19 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector:'app-favorites',
+
+  selector:'favorites',
   templateUrl:'./favorites.component.html',
   styleUrls:['./favorites.component.css']
 })
+
 export class FavoritesComponent{
 
+  danhsach:any =[
+    {name:'Gemma Roberson',Dc1:'Apology for late respone email',Text:'Hello Colette Wooten,'},
+    {name:'Ann Garza',Dc1:'Application for Job Title',Text:'Hello Colette Wooten,'},
+    {name:'Alfonso Burnett',Dc1:'Anything I can help with',Text:'Hello Colette Wooten,'},
+    {name:'Rogan Espinoza',Dc1:'Assistant Marketing Department',Text:'Hello Colette Wooten,'},
+    {name:'Sierra Kerr',Dc1:'Application for Transfer',Text:'Hello Colette Wooten,'}
+  ];
 }
